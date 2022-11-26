6c5146763f5d5efb3f9fafa9aef5930cf9d3d3e1
Pragadesh BS <pragadeshbs@protonmail.com>
2022-11-26 16:35:15 +0530

    remove unused imports

--- dns/DnsServer.java
+++ dns/DnsServer.java
@@ -1,13 +1,9 @@
-
-import java.io.*;
 import java.net.*;
-import java.util.*;
 
 class DNSServer {
     public static void main(String args[]) throws Exception {
         DatagramSocket server = new DatagramSocket(1309);
         while (true) {
-            int flag = 0;
             byte[] sendbyte = new byte[1024];
             byte[] receivebyte = new byte[1024];
             DatagramPacket receiver = new DatagramPacket(receivebyte, receivebyte.length);
@@ -15,10 +11,11 @@ class DNSServer {
             String str = new String(receiver.getData());
             String s = str.trim();
             InetAddress addr = receiver.getAddress();
-            int port = receiver.getPort();    
+            int port = receiver.getPort();
             sendbyte = InetAddress.getByName(s).toString().getBytes();
             DatagramPacket sender = new DatagramPacket(sendbyte, sendbyte.length, addr, port);
-            server.send(sender); break;
+            server.send(sender);
+            break;
         }
         server.close();
     }
